1f3cf4602127889f18e20661c8218baba2b9114c
JevgenijsKonevs <jevgenijskonevs@gmail.com>
2020-12-04 18:12:50 +0200

    Player follow camera / playermovement

---
+++ Assets/Scripts/Combat/Fighter.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+using RPG.Movement;
+
+
+namespace RPG.Combat
+{
+    public class Fighter : MonoBehaviour
+    {
+        [SerializeField] float weaponRange = 2f;
+        Transform target;
+        private void Update()
+        {
+            if (target != null)
+            {
+                GetComponent<Mover>().MoveTo(target.position);
+            }
+        }
+        public void Attack(CombatTarget combatTarget)
+        {
+            target = combatTarget.transform;
+        }
+    }
+}
