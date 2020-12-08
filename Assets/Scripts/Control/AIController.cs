b2d19b21ee3199881c52e21a81058107b783859f
JevgenijsKonevs <jevgenijskonevs@gmail.com>
2020-12-08 19:50:09 +0200

    enemy patrol path

--- Assets/Scripts/Control/AIController.cs
+++ Assets/Scripts/Control/AIController.cs
@@ -4,18 +4,24 @@ using UnityEngine;
 using RPG.Combat;
 using RPG.Core;
 using RPG.Movement;
+
 namespace RPG.Control
 {
     public class AIController : MonoBehaviour
     {
         [SerializeField] float chaseDistance = 5f;
         [SerializeField] float suspicionTime = 3f;
+        [SerializeField] PatrolPath patrolPath;
+        [SerializeField] float waypointTolerance = 3f;
+
         Fighter fighter;
         GameObject player;
         Health health;
         Mover mover;
         Vector3 guardPosition;
+        
         float timeSinceLastSawPlayer = Mathf.Infinity;
+        int currentWaypointIndex = 0;
         private void Start()
         {
             fighter = GetComponent<Fighter>();
@@ -46,7 +52,7 @@ namespace RPG.Control
             else
             {
                 // return to the guarding position
-                GuardBehaviour();
+                PatrolBehaviour();
             }
             // update the time
             timeSinceLastSawPlayer += Time.deltaTime;
@@ -59,9 +65,31 @@ namespace RPG.Control
         {
             GetComponent<ActionSchedule>().CancelCurrentAction();
         }
-         private void GuardBehaviour()
+         private void PatrolBehaviour()
         {
-            mover.StartMoveAction(guardPosition);
+            Vector3 nextPosition = guardPosition;
+            if (patrolPath != null)
+            {
+                if (AtWaypoint())
+                {
+                    CycleWaypoint();
+                }
+                nextPosition = GetCurrentWaypoint();
+            }
+            mover.StartMoveAction(nextPosition);
+        }
+        private bool AtWaypoint()
+        {
+            float distanceToWaypoint = Vector3.Distance(transform.position, GetCurrentWaypoint());
+            return distanceToWaypoint < waypointTolerance;
+        }
+        private void CycleWaypoint()
+        {
+            currentWaypointIndex = patrolPath.GetNextIndex(currentWaypointIndex);
+        }
+        private Vector3 GetCurrentWaypoint()
+        {
+            return patrolPath.GetWaypoint(currentWaypointIndex);
         }
 
         private bool InAttackRangeOfPlayer()
---
+++ Assets/Scripts/Control/PatrolPath.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace RPG.Control
+{
+    public class PatrolPath : MonoBehaviour
+    {   // visualize the patrol path of the enemy 
+        private void OnDrawGizmos()
+        {
+            const float waypointGizmoRadius = 0.3f;
+            // iterate through all waypoints of PathPatrol component in Unity
+            for (int i = 0; i < transform.childCount; i++)
+            {
+                int j = GetNextIndex(i);
+                // draw a waypoint in Unity
+                Gizmos.DrawSphere(GetWaypoint(i), waypointGizmoRadius);
+                // draw lines between waypoints
+                Gizmos.DrawLine(GetWaypoint(i), GetWaypoint(j));
+            }
+        }
+        // getting the index of waypoints of patrol path
+        public int GetNextIndex(int i)
+        {
+            // when the last waypoint is patrolled - return to the first one (line 28)
+            if (i + 1 == transform.childCount)
+            {
+                return 0;
+            }
+            return i + 1;
+        }
+
+        public Vector3 GetWaypoint(int i)
+        {
+            return transform.GetChild(i).position;
+        }
+    }
+}
