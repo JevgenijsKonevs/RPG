5c5b426caa5a1fbcd8ef6f7a99c2dc564412d683
JevgenijsKonevs <jevgenijskonevs@gmail.com>
2020-12-23 21:55:11 +0200

    projectile dealing damage to the enemy and then disappears / shooting animation

--- Assets/Scripts/Combat/Projectile.cs
+++ Assets/Scripts/Combat/Projectile.cs
@@ -1,15 +1,17 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using RPG.Core;
 
 
 namespace RPG.Combat
 {
     public class Projectile : MonoBehaviour
     {
-        [SerializeField] Transform target = null;
-        [SerializeField] float speed = 1f;
 
+        [SerializeField] float speed = 1f;
+        Health target = null;
+        float damage = 0;
 
         // Update is called once per frame
         void Update()
@@ -20,12 +22,27 @@ namespace RPG.Combat
             // move the projectile with the given speed to the targets position
             transform.Translate(Vector3.forward * speed * Time.deltaTime);
         }
+        public void SetTarget(Health target, float damage)
+        {
+            this.target = target;
+            this.damage = damage;
+        }
         private Vector3 GetAimLocation()
         {
             CapsuleCollider targetCapsule = target.GetComponent<CapsuleCollider>();
-            if (targetCapsule == null) return target.position;
+            if (targetCapsule == null) return target.transform.position;
             // return targets position + half ot the targets height
-            return target.position + Vector3.up * targetCapsule.height / 2;
+            return target.transform.position + Vector3.up * targetCapsule.height / 2;
+        }
+        private void OnTriggerEnter(Collider other)
+        {
+            if (other.GetComponent<Health>() != target)
+            {
+                return;
+            }
+            target.TakeDamage(damage);
+            // destroy the arrow that was shot
+            Destroy(gameObject);
         }
     }
 }
